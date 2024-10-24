6907e49c57be521eb970587d790bc3fb81fcf836
Shivar Vishal Pillay <shivar.vishal.pillay@gmail.com>
2024-10-24 08:21:35 -0700

    socialmedia lnk

--- index.js
+++ index.js
@@ -506,7 +506,7 @@ app.put('/api/profile/:id', upload.single('profilePhoto'), async (req, res) => {
             'profile.$[profileElem].position': req.body.position,
             'profile.$[profileElem].company': req.body.company,
             'profile.$[profileElem].about': req.body.about,
-            'profile.$[profileElem].socialMedia': socialMediaLinks,
+            'profile.$[profileElem].socialMedia': socialMediaLinks, // Directly set the entire socialMedia array
             'profile.$[profileElem].colours.$[colourElem].primaryColour': req.body.primaryColour,
             'profile.$[profileElem].colours.$[colourElem].cardColour': req.body.cardColour,
         };
